a884e9ce1909f8a5e2c4416e9bea126753ccc032
Ariane <tianany2@hotmail.com>
2015-07-19 13:31:02 +0100

    Initial Commit

---
+++ wildlifeQuiz/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  wildlifeQuiz
+//
+//  Created by Matt Ariane Clarke on 19/07/2015.
+//  Copyright (c) 2015 Matthew Clarke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
